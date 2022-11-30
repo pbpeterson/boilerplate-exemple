3d68b72c0ab771aa6fe69f390dd334d40ad39395
Peterson Pereira Bozza <petersonbozza7@gmail.com>
2022-11-30 07:16:45 -0300

    feat: add favicon

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,5 @@
 import type { AppProps } from 'next/app';
+import Head from 'next/head';
 import { ThemeProvider } from 'styled-components';
 import { GlobalStyles } from '../styles/globalStyles';
 import theme from '../styles/theme';
@@ -6,6 +7,9 @@ import theme from '../styles/theme';
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider theme={theme}>
+      <Head>
+        <title>Guidance - Boilerplate</title>
+      </Head>
       <Component {...pageProps} />;
       <GlobalStyles />
     </ThemeProvider>
