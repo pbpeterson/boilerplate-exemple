19348c261405f1d8d270dd2e382a478d4d8be8ff
Peterson Pereira Bozza <petersonbozza7@gmail.com>
2022-11-30 05:55:25 -0300

    refactor: move logo to its own component

---
+++ src/components/Logo/index.tsx
@@ -0,0 +1,18 @@
+export const Logo = () => {
+  return (
+    <svg
+      width="200"
+      height="200"
+      viewBox="0 0 51 50"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <circle cx="26" cy="25" r="23" stroke="#272f9b" strokeWidth="4" />
+      <path
+        d="M41.5 10L6 26L25 28.5L28 43.5L41.5 10Z"
+        stroke="#272f9b"
+        strokeWidth="3"
+      />
+    </svg>
+  );
+};
