c8917e1ffe8a403ba79ed79f1e2453f0c6f8d188
Peterson Pereira Bozza <petersonbozza7@gmail.com>
2022-11-30 06:37:12 -0300

    refactor: update imports

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,18 @@
-import { HomeTemplate } from '../templates/Home';
+import { GetStaticProps } from 'next';
+import { HomeTemplate } from '@/templates/Home/index';
 
-export default function Home() {
-  return <HomeTemplate />;
+export type HomeProps = {
+  textInfo: string;
+};
+
+export default function Home({ textInfo }: HomeProps) {
+  return <HomeTemplate textInfo={textInfo} />;
 }
+
+export const getStaticProps: GetStaticProps = () => {
+  return {
+    props: {
+      textInfo: 'We will change your life with technology',
+    },
+  };
+};
--- src/templates/Home/index.tsx
+++ src/templates/Home/index.tsx
@@ -1,13 +1,14 @@
-import { Logo } from '../../components/Logo';
+import { Logo } from '@/components/Logo/index';
+import type { HomeProps } from '@/pages/index';
 import * as S from './styles';
 
-export const HomeTemplate = () => {
+export const HomeTemplate = ({ textInfo }: HomeProps) => {
   return (
     <S.Wrapper>
       <S.LogoWrapper>
         <Logo />
       </S.LogoWrapper>
-      <S.WrapperTexts>We will change your life with technology!</S.WrapperTexts>
+      <S.WrapperTexts>{textInfo}</S.WrapperTexts>
     </S.Wrapper>
   );
 };
