905d665d0aae332f7fff26a648e35c8548d275c6
Peterson Pereira Bozza <petersonbozza7@gmail.com>
2022-11-30 05:56:50 -0300

    feat: add initial theme

---
+++ src/styles/theme.ts
@@ -0,0 +1,15 @@
+export default {
+  colors: {
+    primary: '#ffd135',
+    secondary: '#39009c',
+  },
+  spacings: {
+    xxSmall: '0.8rem',
+    xSmall: '1.6rem',
+    small: '2.4rem',
+    medium: '3.2rem',
+    large: '4.0rem',
+    xLarge: '4.8rem',
+    xxLarge: '5.6rem',
+  },
+};
